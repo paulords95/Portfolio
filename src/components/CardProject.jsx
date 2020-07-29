7b58be9c4857a44eccb0830a2a82ba0ce3729b5a
paulords95 <paulords95@gmail.com>
2020-07-29 17:34:43 -0300

    fixed logos position after animation

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -6,7 +6,7 @@ import "aos/dist/aos.css";
 
 const CardProject = (props) => {
   useEffect(() => {
-    Aos.init({ duration: 2000 });
+    Aos.init({ duration: 3000 });
   }, []);
   return (
     <div data-aos="fade-up" data-aos-duration="1000" className="projectCard">
