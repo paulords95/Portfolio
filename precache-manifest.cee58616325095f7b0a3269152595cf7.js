bdaabd9d010eab0852e11cdec8817d2910e8339d
paulords95 <paulords95@gmail.com>
2020-09-29 15:28:12 -0300

    Updates

---
+++ precache-manifest.cee58616325095f7b0a3269152595cf7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34079bb0a016cf960309ba3c6ca92aaa",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "bcc44fa3bbc7ab958bd4",
+    "url": "/Portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "1da17f856d287e886d5e",
+    "url": "/Portfolio/static/css/main.0b0d1148.chunk.css"
+  },
+  {
+    "revision": "bcc44fa3bbc7ab958bd4",
+    "url": "/Portfolio/static/js/2.240e367d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.240e367d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1da17f856d287e886d5e",
+    "url": "/Portfolio/static/js/main.12366b20.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "2d5d55c56c33d3e8b7600674f73ddb71",
+    "url": "/Portfolio/static/media/download.2d5d55c5.svg"
+  },
+  {
+    "revision": "c03f372407626b0def18968edb93f92c",
+    "url": "/Portfolio/static/media/winxp.c03f3724.PNG"
+  }
+]);
