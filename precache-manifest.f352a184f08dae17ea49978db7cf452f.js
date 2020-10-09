ae1172ec05eb72e3f5acd763e8a87e813acb54dd
paulords95 <paulords95@gmail.com>
2020-10-08 22:24:34 -0300

    Updates

---
+++ precache-manifest.f352a184f08dae17ea49978db7cf452f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d801f208e0e6dcce0e58cd8d1988040",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40eb14c101b36d2444ed",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "aad7a64c6009d9d672dc",
+    "url": "/static/css/main.4d5ff539.chunk.css"
+  },
+  {
+    "revision": "40eb14c101b36d2444ed",
+    "url": "/static/js/2.f87654d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f87654d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad7a64c6009d9d672dc",
+    "url": "/static/js/main.d2177113.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "2d5d55c56c33d3e8b7600674f73ddb71",
+    "url": "/static/media/download.2d5d55c5.svg"
+  }
+]);
