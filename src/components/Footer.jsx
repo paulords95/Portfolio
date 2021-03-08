be29a1132a67aef544b28a82b36d5f0dfd29d9a5
paulords95 <paulords95@gmail.com>
2021-03-08 18:48:00 -0300

    fixed positioning

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,12 +6,12 @@ const Footer = () => {
   return (
     <footer id="footerSection" className="footer">
       <div className="footer-info">
-        <h1 className="footer-title">Contato</h1>
+        <h1 className="footer-title"></h1>
         <hr></hr>
         <ul className="info-list">
           <li>contato@paulo.dev.br</li>
         </ul>
-        <hr style={{ marginTop: 35 }}></hr>
+        <hr style={{ marginTop: 40 }}></hr>
         <div className="footer-logos"></div>
         <h1 className="year">2021</h1>
       </div>
